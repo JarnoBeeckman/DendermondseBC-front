a0f548cb9bd6a66289f2ffe571f946ca799dd5fe
Jarno Beeckman <71630651+JarnoBeeckman@users.noreply.github.com>
2022-03-10 20:20:33 +0100

    login screen update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form"
 import { useLogin, useSession } from "../context/AuthProvider";
 import { useHistory } from "react-router-dom";
+import './login.css'
 
 
 export default function Login() {
@@ -23,23 +24,31 @@ export default function Login() {
         }
     },[login,history])
 
-    return (<>
-            <div className="mx-auto">
-                <h1 className="">Log in</h1>
-                {error ? (<p className="text-red-500 errorlogin">{JSON.stringify(error)}</p>): null}
-                <form className="grid grid-cols-1 login" onSubmit={handleSubmit(handleLogin)}>
-                    <label htmlFor="mail">email:</label>
-                    <input id="mail" type="text" defaultValue="" data-cy='mail' placeholder="your@mail.com" {...register('mail',{required: 'This is required'})}/>
-                    {errors.mail && <p className="text-red-500">{errors.mail.message}</p>}
-                    <label htmlFor="wachtwoord">wachtwoord:</label>
-                    <input id="pw" type="password" data-cy='wachtwoord'placeholder="wachtwoord" {...register('wachtwoord',{required: 'This is required'})}/>
-                    {errors.wachtwoord && <p className="text-red-500">{errors.wachtwoord.message}</p>}
-                    <div className="loginbutton">
-                        <button type="submit" className="disabled:opacity-50 lbut " disabled={loading} data-cy='login' >Log in</button>
+    return (
+            <div className="limit">
+                <div className="conlogin">
+                    <div className="wraplogin">
+                        {error ? (<p className="text-red-500 errorlogin">{JSON.stringify(error)}</p>): null}
+                        <form className="loginform validate-form flex-sb flex-w" onSubmit={handleSubmit(handleLogin)}>
+                            <span className="loginformtitle">Login</span>
+                            <div className="wrapinput validate-input">
+                                <input id="mail" className="input" type="text" defaultValue="" data-cy='mail' placeholder="mail" {...register('mail',{required: 'Dit is vereist'})}/>
+                                <span className="focusinput"></span>
+                            </div>
+                            {errors.mail && <p>{errors.mail.message}</p>}
+                            <div className="wrapinput validate-input">
+                                <input id="pw" className="input" type="password" data-cy='wachtwoord'placeholder="wachtwoord" {...register('wachtwoord',{required: 'Dit is vereist'})}/>
+                                <span className="focusinput"></span>
+                            </div>
+                            {errors.wachtwoord && <p>{errors.wachtwoord.message}</p>}
+                            <div className="conloginform">
+                                <button type="submit" className="loginbutton" disabled={loading} data-cy='login' >Log in</button>
+                                {console.log(loading)}
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
-            </>
     )
 
 }
