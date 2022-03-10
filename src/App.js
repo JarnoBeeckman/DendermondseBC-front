e1bc231fc4eca3921fac7a5f0c4c4b1cc7897de2
Jarno Beeckman <71630651+JarnoBeeckman@users.noreply.github.com>
2022-03-10 18:18:53 +0100

    redirect github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import PrivateRoute from "./components/PrivateRoute";
 import Main from './pages/main';
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
       <Switch>
         <Route exact path='/login'><Login /></Route>
+        <Route exact path='/DendermondseBC-front'><Redirect to='/' /></Route>
         <PrivateRoute exact path='/'><Main /></PrivateRoute>
       </Switch>
       </BrowserRouter>
