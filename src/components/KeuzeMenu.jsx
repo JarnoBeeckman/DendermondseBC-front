d1994c6af656bd414d6a0ca91f9a9a442b478ff0
Jarno Beeckman <71630651+JarnoBeeckman@users.noreply.github.com>
2022-03-13 23:55:13 +0100

    responsive fix + logout

--- src/components/KeuzeMenu.jsx
+++ src/components/KeuzeMenu.jsx
@@ -1,18 +1,24 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { useSession } from "../context/AuthProvider";
 import config from '../config.json'
+import { useLogout } from "../context/AuthProvider";
 
 export default function KeuzeMenu() {
    const {lid,loading,error} = useSession();
+   const logout = useLogout();
+
+   const loguit = useCallback(async ()=>{
+    await logout()
+   },[logout])
 
    const Buttn = memo((props)=>{
-    return <button className="keuzemenubutton" key={props.page}>{props.page}</button>
+    return <button className="keuzemenubutton" key={props.page} onClick={props.click}>{props.page}</button>
    })
 
     if (lid) {
         if (!lid.roles.includes('beheerder')) {
         return (
-        <div className="buttongrid flex-w flex-sb">
+        <div className="buttongrid flex-w">
             {lid.roles.map(role=>{
                const x = config.pages[role]
                return x.map(page=>{
@@ -20,12 +26,14 @@ export default function KeuzeMenu() {
                 return <Buttn key={page} page={page}/>
                     })
             })}
+            <Buttn />
         </div>
         ) }
-        return (<div className="buttongrid flex-w flex-sb">
+        return (<div className="buttongrid flex-w">
         {config.pages.beheerder.map(page=>{
             return <Buttn key={page} page={page}/>
         })}
+        <Buttn key={'Log uit'} page={"Log uit"} click={loguit} />
         </div>);
     }
     if (loading) 
