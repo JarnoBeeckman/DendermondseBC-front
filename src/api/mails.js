ceea468b255bcbab2839ea444fe60d24f5efde12
Jarno Beeckman <71630651+JarnoBeeckman@users.noreply.github.com>
2022-04-14 01:54:07 +0200

    mails subject & receivers

--- src/api/mails.js
+++ src/api/mails.js
@@ -1,6 +1,14 @@
 import {axios} from ".";
 
-export const sendMail = async ()=>{
-    const {data} = axios.get()
+export const sendMail = async (list,cc,bcc,onderwerp,text,bijlagen)=>{
+    const {data} = await axios.post('mails/',{
+        ontvangers: list,
+        cc: cc,
+        bcc:bcc,
+        onderwerp:onderwerp,
+        text:text,
+        bijlagen:bijlagen
+    })
+
     return data
 }
--- src/pages/mails.jsx
+++ src/pages/mails.jsx
@@ -2,9 +2,13 @@ import { useState, useCallback, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useGetAllLeden,useSession } from "../context/AuthProvider"
 import * as mails from '../api/mails'
+import * as groep from '../api/groep'
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header'; 
 import List from '@editorjs/list'; 
+import SimpleImage from '@editorjs/simple-image'
+import { useForm } from "react-hook-form"
+
 
 let editor;
 
@@ -12,10 +16,13 @@ export default function Mails() {
 
     const {ready} = useSession()
     const [leden,setLeden] = useState()
+    const [groepen,setGroepen] = useState()
     const history = useHistory()
     const [loading,setLoading] = useState()
     const [customError,setCustomError]=useState();
     const list = useGetAllLeden()
+    const { register, handleSubmit, formState: {errors} } = useForm();
+    const [receivers,setReceivers] = useState([])
 
     const back = useCallback(async ()=>{
         history.push('/')
@@ -27,7 +34,12 @@ export default function Mails() {
         if (e === 404) setCustomError('Kon leden niet laden')
         else {
             setLeden(e)
-            setCustomError(null)
+            const a = await groep.getAll()
+            if (a === 404) setCustomError('Kon groepen niet laden')
+            else {
+                setCustomError(null)
+                setGroepen(a)
+            }
         } 
         setLoading(false)
     },[list])
@@ -37,46 +49,51 @@ export default function Mails() {
             refresh()
     },[ready,refresh])
 
-    const send = useCallback(async ()=>{
+    const send = useCallback(async ({onderwerp})=>{
         setLoading(true)
-        const e = await mails.sendMail()
+        const data = await editor.save().then(x=>{return x}).catch(x=>setCustomError('Kon gegevens niet verwerken.'))
+        const e = await mails.sendMail(receivers,false,true,onderwerp,data.blocks)
         if (!e) setCustomError('Kon mail niet versturen')
         setLoading(false)
-    },[])
+    },[receivers])
 
-    
+    const filter = useCallback((id)=>{
+        const e = receivers
+        if (e.length !== 1) {
+             e.splice(e.indexOf(id),1)
+             setReceivers([...e])
+        } else
+        return setReceivers([])
+    },[receivers])
 
-    if (ready && leden) {
+    if (ready && leden && groepen) {
         if (!editor)
         editor = new EditorJS({ 
-            /** 
-             * Id of Element that should contain the Editor 
-             */ 
             holder: 'editorjs', 
-          
-            /** 
-             * Available Tools list. 
-             * Pass Tool's class or Settings object for each Tool you want to use 
-             */ 
-            tools: { 
-              header: {
-                class: Header, 
-                inlineToolbar: ['link'] 
+             tools: { 
+                header: Header, 
+                list: List,
+                image: SimpleImage,
               }, 
-              list: { 
-                class: List, 
-                inlineToolbar: true 
-              } 
-            }, 
           })
     return <>
         <button className='backbutton margin20' onClick={back}>{'<'} Terug</button>
         {customError ? (<p className="error">{customError}</p>): null}
-
-        <div id="editorjs" className="editor"></div>   
-        
-        <button disabled={loading} className="wwwijzig" onClick={()=>send()}>Versturen</button>
-
+          <form onSubmit={handleSubmit(send)} className='grid flex-w justify fullwidth'>
+            <label className="acclabel">Ontvangers: </label>
+            <div className="accvalue flex-w">
+                <label className="radiolabel"><input type='radio' checked={receivers.includes(-2)} onChange={()=>null} onClick={()=> receivers.includes(-2) ? setReceivers([]) : setReceivers([-2])} />Iedereen</label>
+                <label className="radiolabel"><input type='radio' checked={receivers.includes(-1)} onChange={()=>null} onClick={()=> receivers.includes(-1) ? setReceivers([]) : setReceivers([-1])} />Actieve leden</label>
+                {groepen.map(x=>{
+                    return <label className="radiolabel" key={x.gid}><input type='radio' disabled={receivers.includes(-1) || receivers.includes(-2)} checked={receivers.includes(x.gid)} onChange={()=>null} onClick={()=>receivers.includes(x.gid) ? filter(x.gid) : setReceivers([...receivers,x.gid])} />{x.groepnaam}</label>
+                })}
+            </div>
+            <label className="acclabel">Onderwerp: </label>
+            <input className="accvalue inputfix" {...register('onderwerp',{required: 'Dit is vereist'})}/>
+            {errors.onderwerp && <><div className='acclabel'></div><p className='accvalue error' >{errors.onderwerp.message}</p></>}
+            <div id="editorjs" className="editor"/> 
+            <button disabled={loading} className="wwwijzig" type="submit">Versturen</button>
+        </form>
     </>
     }
     return <><button className='backbutton margin20' onClick={back}>{'<'} Terug</button><div>Loading...</div></>
